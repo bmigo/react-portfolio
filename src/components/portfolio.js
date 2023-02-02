10ffda94dbb1a277d28707c703c0fea8dd6638e6
bmigo <berniecmigo@gmail.com>
2023-02-02 14:09:18 -0500

    portfolio.js

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// EDIT SO THAT REACT WORKS
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Portfolio = () => {
+  return (
+    <body>
+    <header>
+        <h1>Hello, I'm Bernadette Migo</h1>
+        <nav class="topnav">
+        <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#work">Work</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+        </nav>
+    </header>
+    <br>
+    <section id="about" class="about">
+        <h2>About Me</h2>
+        <p>My name is Bernadette Migo. I'm a junior software developer centered in the greater Richmond area</p>
+        <img src="./assets/images/bernie.jpeg" alt="Picture of Bernadette Migo" id="bernieImg">
+    </section>
+
+    <section id="work" class="work">
+        <h3>My Work</h3>
+        <div>
+            <div class=>
+                <a href="https://github.com/bmigo/Horiseon"><img src="./assets/images/horiseon.jpg" id="horiseonImg"></a>
+            </div class="row">
+            <div>
+                <div class="box">HTML</div>
+                <div class="box">CSS</div>
+                <div class="box">JavaScript</div>
+            </div>
+        </div>
+        
+    </section>
+  )
+}
